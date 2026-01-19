bd258fe849db3c299bf1ba68e4b05daf47f7d30c
mr7aali <mr7aali@gmail.com>
2026-01-20 04:23:17 +0600

    migration added

---
+++ prisma/migrations/20260119222220_otp_modal_added/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "password_reset_otp" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "otp" TEXT NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "password_reset_otp_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "password_reset_otp_email_idx" ON "password_reset_otp"("email");
+
+-- CreateIndex
+CREATE INDEX "password_reset_otp_email_otp_idx" ON "password_reset_otp"("email", "otp");
